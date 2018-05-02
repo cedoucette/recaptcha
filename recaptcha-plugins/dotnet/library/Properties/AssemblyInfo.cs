44e857f1204b77da140a1c562a10667a5de2fa03
Charles Doucette <Charles.Doucette@hmhco.com>
2018-05-02 13:10:24 -0400

    Increment version of assembly

--- recaptcha-plugins/dotnet/library/Properties/AssemblyInfo.cs
+++ recaptcha-plugins/dotnet/library/Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Web.UI;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.6.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.7.0")]
+[assembly: AssemblyFileVersion("1.0.7.0")]
 
 [assembly: AllowPartiallyTrustedCallers]
 //[assembly: SecurityRules(SecurityRuleSet.Level1)]
